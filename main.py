8a45ff74c0f5680f06c3c265bd55371aa6fd0c93
V4nch <idmo_moreno@hotmail.com>
2025-04-10 16:41:10 -0500

    testing git works fine

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def BST():
+   arbol=[5,3,2,4,12,9,8,11,14,13,19]
+   nodo=[]
+    for i in arbol:
+        
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
